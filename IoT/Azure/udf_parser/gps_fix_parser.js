ab06a78f2b6174b6e260d6739b2f74ec8905aba5
CodingIsLove <christopher.germann.eth@gmail.com>
2021-05-05 09:50:31 +0200

    gps_parser for abeeway udf done

--- IoT/Azure/udf_parser/gps_fix_parser.js
+++ IoT/Azure/udf_parser/gps_fix_parser.js
@@ -1,24 +1,70 @@
-let gps_payload = '035cc49220001c3d280516f902b7d18e'
 
 function main(hex_payload) {
-    if (hex_payload.slice(0, 2) == '03' && getPosType(hex_payload.slice(9,10) == 'GPS fix')) {
-        let parsedContent = [];
-
+    let parsedContent = {};
+    let post_type = getPosType(hex_payload.slice(9, 10))
+    if (hex_payload.slice(0, 2) === '03' && post_type === 'GPS fix') {
+        console.log('It is a position message')
         // Parse common header
-        parsedContent[0] = 'Position Message'; //
-        parsedContent[1] = getBatteryStatus(hex_payload.slice(4, 6))
-        parsedContent[2] = getTemperature(hex_payload.slice(6, 8))
-        parsedContent[3] = getAck(hex_payload.slice(8, 9))
-        parsedContent[4] = getPosType(hex_payload.slice(9, 10))
+
+        parsedContent.msgType= 'Position Message'
+        parsedContent.battery = getBatteryStatus(hex_payload.slice(4, 6))
+        parsedContent.tmp = getTemperature(hex_payload.slice(6, 8))
+        parsedContent.ack = getAck(hex_payload.slice(8, 9))
+        parsedContent.posType = getPosType(hex_payload.slice(9, 10))
 
         // GPS fix specific data
-        //parsedContent[] //todo add later on
+        parsedContent.age = getAge(hex_payload.slice(10, 12))
+        parsedContent.lat = getLatitude(hex_payload.slice(12, 18))
+        parsedContent.lon = getLongitude(hex_payload.slice(18, 24))
+        parsedContent.ehpe = getEHPE(hex_payload.slice(24, 26))
+
+        return parsedContent
 
     } else {
-        return []
+        return parsedContent
     }
 }
 
+
+function getAge(byte) {
+    return generalValueDecoder(
+        parseInt(byte, 16),
+        0,
+        2040,
+        8,
+        0
+    )
+}
+
+//Algorithm for Latitude can be found in the abeeway micro tracker reference guide v.1.8_05 => page 27 => FYI, there is a error in the documentation, the exponent should be 10e6 not 10e7
+function getLatitude(byte) {
+    let lat = parseInt(byte, 16) << 8
+    if (lat > parseInt('7FFFFFFF', 16)) {
+        lat = lat - parseInt('1000000000')
+    }
+    lat = lat / 10e6
+
+    return lat
+}
+
+function getLongitude(byte) {
+    let lon = parseInt(byte, 16) << 8
+    if (lon > parseInt('7FFFFFFF', 16)) {
+        lon = lon - parseInt('1000000000')
+    }
+    lon = lon / 10e6
+    return lon
+}
+
+function getEHPE(byte) {
+    return generalValueDecoder(
+        parseInt(byte, 16),
+        0,
+        1000,
+        8,
+        0
+    )
+}
 
 // byte should be in form of 0xbb
 function getBatteryStatus(byte) {
@@ -27,25 +73,24 @@ function getBatteryStatus(byte) {
     } else if (byte == 'ff') {
         return 'measurement_error'
     } else {
-        return generalValueDecoder(
+        return Number(generalValueDecoder(
             parseInt(byte, 16),
             2.8,
             4.2,
             8,
             5.5
-        )
+        )).toFixed(4)
     }
 }
 
 // byte should be in form of 0xbb
 function getTemperature(byte) {
-    return generalValueDecoder(parseInt(byte,16),
+    return Number(generalValueDecoder(parseInt(byte, 16),
         -44,
         85,
         8,
-        0,
-        0.5
-        )
+        0
+    )).toFixed(2)
 }
 
 function getAck(payload) {
@@ -93,9 +138,3 @@ const generalStepSizeDecoder = function (lo, hi, nbits, nresv) {
 const generalValueDecoder = function (value, lo, hi, nbits, nresv) {
     return ((value - nresv / 2) * generalStepSizeDecoder(lo, hi, nbits, nresv) + lo)
 }
-
-
-// Execute the parser
-let result = main(gps_payload)
-
-console.log(`${result.forEach(element => console.log(element))}`)
